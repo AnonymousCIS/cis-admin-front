9005785055d72a662ffba68affb87376c0ad3b32
BDloverHS <gustn020702naver.com@gmail.com>
2025-02-12 23:03:22 +0900

    loan 중간 커밋

--- src/app/loan/components/LoanList.tsx
+++ src/app/loan/components/LoanList.tsx
@@ -1,11 +1,49 @@
 import React from 'react'
 import styled from 'styled-components'
 import { TableRows } from '@/app/global/components/Tables'
+import { SmallButton } from '@/app/global/components/Buttons'
 import { MdCheckBoxOutlineBlank } from 'react-icons/md'
 
 const StyledForm = styled.form``
 
-const LoanListForm = () => {
+const LoanItem = ({ item }) => {
+  const {
+    loanName,
+    bankName,
+    category,
+    limit,
+    interestRate,
+    repaymentYear,
+    isOpen,
+  } = item
+
+  return (
+    <tr>
+      <td></td>
+      <td>{loanName}</td>
+      <td>{bankName}</td>
+      <td>{category}</td>
+      <td>{limit}</td>
+      <td>{interestRate}</td>
+      <td>{repaymentYear}</td>
+      <td>{isOpen}</td>
+      <td>
+        <a href="/">
+          <SmallButton type="button" color="info" width={120}>
+            수정
+          </SmallButton>
+        </a>
+        <a href="/">
+          <SmallButton type="button" color="danger" width={120}>
+            삭제
+          </SmallButton>
+        </a>
+      </td>
+    </tr>
+  )
+}
+
+const LoanList = ({ items }) => {
   return (
     <>
       <StyledForm>
@@ -21,14 +59,22 @@ const LoanListForm = () => {
               <th>최대 한도</th>
               <th>금리</th>
               <th>상환년도</th>
+              <th>사용 가능 여부</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td colSpan={7} className="no-data">
-                조회 게시판이 없습니다.
-              </td>
-            </tr>
+            {items && items.length > 0 ? (
+              items.map((item) => (
+                <LoanItem key={'config_' + item.bid} item={item} />
+              ))
+            ) : (
+              <tr>
+                <td colSpan={9} className="no-data">
+                  조회 게시판이 없습니다.
+                </td>
+              </tr>
+            )}
           </tbody>
         </TableRows>
       </StyledForm>
@@ -36,4 +82,4 @@ const LoanListForm = () => {
   )
 }
 
-export default React.memo(LoanListForm)
+export default React.memo(LoanList)
