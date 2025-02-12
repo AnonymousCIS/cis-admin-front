dca162d285400c0821a074e68bc3e6c69cc9a5f3
PaB1010 <mudodd@naver.com>
2025-02-12 12:43:48 +0900

    fix port

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: 'admin-cis-pintech',
       script: 'node_modules/next/dist/bin/next',
-      args: 'start -p 6000',
+      args: 'start -p 7000',
       instances: 1,
       exec_mode: 'cluster',
     },
