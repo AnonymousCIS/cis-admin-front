8ee6256ad582e0d0817e26293bac7141a5420c16
kks940104 <kks940104@gmail.com>
2025-02-16 19:20:41 +0900

    .

--- src/app/message/containers/WriteContainer.tsx
+++ src/app/message/containers/WriteContainer.tsx
@@ -3,10 +3,12 @@
 import React from 'react'
 import WriteForm from '../components/WriteForm'
 
-const WriteContainer = () => {
-  return (
-  <WriteForm />
-)
+const WriteContainer = ({
+  email,
+}: { email?: string | undefined } | undefined) => {
+  const name = email.replace('%', '@')
+  console.log('name', name)
+  return <WriteForm />
 }
 
 export default React.memo(WriteContainer)
