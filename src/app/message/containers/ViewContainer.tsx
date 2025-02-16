5ae626a50ee2c90c53a3dae3f914d34ec50179f2
kks940104 <kks940104@gmail.com>
2025-02-16 22:57:56 +0900

    메시지 수정

--- src/app/message/containers/ViewContainer.tsx
+++ src/app/message/containers/ViewContainer.tsx
@@ -4,26 +4,20 @@ import ViewForm from '../components/ViewForm'
 import { getMessage } from '../services/actions'
 import useMenuCode from '@/app/global/hooks/useMenuCode'
 
-const ViewConatiner = ( seq : number) => {
+const ViewConatiner = ({ seq }) => {
   useMenuCode('message', 'view')
-
   const [data, setData] = useState([])
-
   useLayoutEffect(() => {
     ;(async () => {
-      try{
+      try {
         const _data = await getMessage(seq)
         setData(_data)
-        console.log('_data', _data)
-
-      }catch(err) {
+      } catch (err) {
         console.error(err)
         return
       }
-      
-
     })()
-  }, [seq])
+  }, [])
 
   return <ViewForm data={data} />
 }
