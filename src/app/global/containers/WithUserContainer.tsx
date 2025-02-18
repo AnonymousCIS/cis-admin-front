01edf09251b28caad6516bbf9edfdd4c89bb2164
kks940104 <kks940104@gmail.com>
2025-02-18 18:40:03 +0900

    로딩중 추가

--- src/app/global/containers/WithUserContainer.tsx
+++ src/app/global/containers/WithUserContainer.tsx
@@ -2,6 +2,7 @@
 
 'use client'
 
+import { useState, useEffect } from 'react'
 import useUser from '../hooks/useUser'
 import LoginContainer from '@/app/member/containers/LoginContainer'
 import { MainContentBox } from '../components/ContentBox'
@@ -11,6 +12,19 @@ import { unauthorized } from 'next/navigation'
 
 export default function WithUserContainer(UserContainer) {
   // 로그인 상태일때만 가능한 컨테이너 = UserContainer
+
+  const [loading, setLoading] = useState<boolean>(false)
+
+  useEffect(() => {
+    setLoading(true)
+    const timeoutId = setTimeout(function () {
+      setLoading(false)
+    }, 1000)
+
+    return () => {
+      clearTimeout(timeoutId)
+    }
+  }, [])
 
   const { isLogin, isAdmin } = useUser()
 
@@ -25,7 +39,11 @@ export default function WithUserContainer(UserContainer) {
   // 로그인 상태일 경우 현재 컨테이너
   // 로그인 상태가 아닐 경우 로그인 컨테이너쪽 페이지로, 이후 로그인시 원래 접근하려던 주소로 이동
   return isLogin ? (
-    UserContainer
+    loading ? (
+      <div>로딩중...</div>
+    ) : (
+      UserContainer
+    )
   ) : (
     <MainContentBox max={450} min={350}>
       <MainTitle>로그인</MainTitle>
