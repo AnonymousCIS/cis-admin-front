83eef94286a794caac99c1f32f40bae4bd7916a5
junqwer1 <zzang12358@naver.com>
2025-02-19 01:37:29 +0900

    쪽지 삭제, 목록 수정

--- src/app/message/components/ListForm.tsx
+++ src/app/message/components/ListForm.tsx
@@ -8,26 +8,7 @@ import {
   // MdRadioButtonUnchecked,
 } from 'react-icons/md'
 import { SmallButton } from '@/app/global/components/Buttons'
-import { Select } from '@/app/global/components/FormComponents'
 import { CommonType } from '@/app/global/types/StyledType'
-import colors from '@/app/global/styles/colors'
-import classNames from 'classnames'
-const { white, dark, secondary } = colors
-
-const StyledTab = styled.ul<CommonType>`
-  display: inline-flex;
-  cursor: pointer;
-  background: ${white};
-  color: ${dark};
-  .on {
-    background: ${secondary};
-    color: ${white};
-  }
-  li {
-    padding: 12px;
-    border: 1px solid ${secondary};
-  }
-`
 
 const StyledForm = styled.form<CommonType>`
   th:nth-of-type(1) {
@@ -35,11 +16,11 @@ const StyledForm = styled.form<CommonType>`
   }
 
   th:nth-of-type(2) {
-    width: 100px;
+    width: 50px;
   }
 
   th:nth-of-type(3) {
-    width: 120px;
+    width: 100px;
   }
 
   th:nth-of-type(4) {
@@ -47,16 +28,28 @@ const StyledForm = styled.form<CommonType>`
   }
 
   th:nth-of-type(5) {
-    width: 60px;
+    width: 120px;
+  }
+
+  th:nth-of-type(6) {
+    width: 80px;
   }
 
   th:nth-of-type(7) {
+    width: 80px;
+  }
+
+  th:nth-of-type(8) {
+    width: 80px;
+  }
+
+  th:nth-of-type(10) {
     width: 261px;
   }
 `
 
 const ListItem = ({ item, onModal }) => {
-  const { subject, status, content, seq, senderEmail, receiverEmail } = item
+  const { subject, status, content, seq, senderEmail, receiverEmail, deletedBySender, deletedByReceiver, notice } = item
   // console.log('item', item)
 
   return (
@@ -64,10 +57,13 @@ const ListItem = ({ item, onModal }) => {
       <td>
         <MdCheckBoxOutlineBlank />
       </td>
+      <td style={{textAlign: 'center'}}>{notice === true ? 'O' : ''}</td>
       <td>{subject}</td>
       <td>{senderEmail}</td>
       <td>{receiverEmail}</td>
       <td>{status}</td>
+      <td style={{textAlign: 'center'}}>{deletedBySender === false ? 'X' : 'O'}</td>
+      <td style={{textAlign: 'center'}}>{deletedByReceiver === false ? 'X' : 'O'}</td>
       <td>
         <div
           className="content"
@@ -94,7 +90,7 @@ const ListItem = ({ item, onModal }) => {
 }
 
 const ListForm = ({ onModal, items }) => {
-  // console.log('items', items)
+  console.log('items', items)
   return (
     <>
       <StyledForm>
@@ -104,22 +100,25 @@ const ListForm = ({ onModal, items }) => {
               <th>
                 <MdCheckBoxOutlineBlank />
               </th>
+              <th>공지</th>
               <th>제목</th>
               <th>보낸 사람 이메일</th>
               <th>받은 사람 이메일</th>
               <th>열람 여부</th>
+              <th>보낸 사람 삭제 여부</th>
+              <th>받은 사람 삭제 여부</th>
               <th>내용</th>
               <th></th>
             </tr>
           </thead>
           <tbody>
-            {items ? (
+            {items.length ? (
               items.map((item) => (
                 <ListItem key={item?.seq} item={item} onModal={onModal} />
               ))
             ) : (
               <tr>
-                <td colSpan={7} className="no-data">
+                <td colSpan={8} className="no-data">
                   쪽지가 없습니다.
                 </td>
               </tr>
